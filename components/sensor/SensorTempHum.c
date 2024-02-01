a87f6d3c3a46d61663fde7d3f5c2a7544b07b035
AlexCork1 <ales.cep@gmail.com>
2024-02-01 16:19:29 +0100

    fixed deadlock bug

--- components/sensor/SensorTempHum.c
+++ components/sensor/SensorTempHum.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
+#include "esp_log.h"
 
 /*
  * Code where all reading from temperature sensor is done
@@ -12,43 +13,54 @@
 /*
  * Private variables
  */
-SensorData _local;
-SemaphoreHandle_t _sensorDataUpdateSemaphore;
+static SensorData _local;
+static SemaphoreHandle_t _sensorDataUpdateSemaphore;
 const int32_t SemaphoreWait_MS = 500 / portTICK_PERIOD_MS;
 
 /*
  * Private functions prototypes
  */
+static void SetData(int32_t temp, int32_t hum);
 int32_t GetRandom(int32_t MIN, int32_t MAX);
 
 void Sensor_Init(uint32_t seed)
 {
-	_sensorDataUpdateSemaphore = xSemaphoreCreateBinary();
+	_sensorDataUpdateSemaphore = xSemaphoreCreateMutex();
+	if(_sensorDataUpdateSemaphore == NULL)
+	{
+        ESP_LOGE("SENSOR DATA", "Semaphore create failed");
+	}
 	srand(seed);
 
-	_local.humidity = GetRandom(0, 100);
-	_local.temperature = GetRandom(-30, 45);
+	SetData(GetRandom(-30, 45), GetRandom(0, 100));
 }
 
 /*
  * This function "reads" new data from temperature sensor
  */
-void Sensor_UpdateData()
+static void SetData(int32_t temp, int32_t hum)
 {
 	if( _sensorDataUpdateSemaphore != NULL )
 	{
 		//get into danger zone
-		if (xSemaphoreTake(_sensorDataUpdateSemaphore, SemaphoreWait_MS) == pdTRUE)
+		if (xSemaphoreTake(_sensorDataUpdateSemaphore, portMAX_DELAY) == pdTRUE)
 		{
-			//replace values
-			_local.humidity = GetRandom(0, 100); //or something
-			_local.temperature = GetRandom(-30, 45); //or something
+			//replace "global" values
+			_local.humidity = hum; //or something
+			_local.temperature = temp;
 
 			//release semaphore
 			xSemaphoreGive(_sensorDataUpdateSemaphore);
-		}
 
+		}
 	}
+}
+void Sensor_UpdateData()
+{
+	int32_t temp = GetRandom(-30, 45);
+	int32_t hum = GetRandom(0, 100);
+
+	SetData(temp, hum);
 }
 void Sensor_GetData(SensorData *data)
 {
@@ -56,7 +68,7 @@ void Sensor_GetData(SensorData *data)
 	if( _sensorDataUpdateSemaphore != NULL )
 	{
 		//get into danger zone
-		if (xSemaphoreTake(_sensorDataUpdateSemaphore, SemaphoreWait_MS) == pdTRUE)
+		if (xSemaphoreTake(_sensorDataUpdateSemaphore, portMAX_DELAY) == pdTRUE)
 		{
 			//overwrite values
 			data->humidity = _local.humidity;
